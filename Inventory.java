4dac38640515c42c1b608d23e3b1246d768af072
Laika May <66331364+ChinamaykzaLaika@users.noreply.github.com>
2022-02-25 21:03:54 -0500

    Update Inventory.java

--- Inventory.java
+++ Inventory.java
@@ -1,7 +1,11 @@
 public class Inventory {
     private int stockSize;
-    private int productPrice;
-    private int totalPurchasePrice;
+    private double productPrice;
+    private double totalPurchasePrice;
+    private double totalBookPrice;
+    private double totalCDPrice;
+    private double totalDVDPrice;
+    private double bookstoreBalance = 0;
 
     public Inventory(int productPrice, int stockSize) {
         this.productPrice = productPrice;
@@ -14,17 +18,42 @@ public class Inventory {
     public void setStockSize(int stockSize) {
         this.stockSize = stockSize;
     }
-    public int getProductPrice() {
+    public double getProductPrice() {
         return productPrice;
     }
     public void setProductPrice(int productPrice) {
         this.productPrice = productPrice;
     }
-    public int getPurchaseTotal(int numBooks, int numCDs, int numDVDs) {
+
+    // Books price total
+    public double getTotalBookPrice() {
+        return totalBookPrice;
+    }
+    public void setTotalBookPrice(int numBooks) {
+        this.totalBookPrice = (7 * numBooks);
+    }
+
+    // CD price total
+    public double getTotalCDPrice() {
+        return totalCDPrice;
+    }
+    public void setTotalCDPrice(int numCDs) {
+        this.totalCDPrice = (12 * numCDs);
+    }
+
+    // DVD price total
+    public double getTotalDVDPrice() {
+        return totalDVDPrice;
+    }
+    public void setTotalDVDPrice (int numDVDs) {
+        this.totalDVDPrice = (18 * numDVDs);
+    }
+
+    public double getPurchaseTotal(int numBooks, int numCDs, int numDVDs) {
         this.totalPurchasePrice = (7 * numBooks) + (12 * numCDs) + (18 * numDVDs);
         return totalPurchasePrice;
     }
-    public void setPurchaseTotal(int totalPurchasePrice) {
+    public void setPurchaseTotal(double totalPurchasePrice) {
         this.totalPurchasePrice = totalPurchasePrice;
     }
 
@@ -35,4 +64,10 @@ public class Inventory {
         System.out.println((7 * numBooks) + (12 * numCDs) + (18 * numDVDs));
     }
 
+    public double getBookstoreBalance() {
+        return bookstoreBalance;
+    }
+    public void setBookstoreBalance(double bookstoreProfit) {
+        this.bookstoreBalance = bookstoreProfit + bookstoreBalance;
+    }
 }
