b33c46260772e40663c72b76d6cc0c4b5e55449a
Laika May <66331364+ChinamaykzaLaika@users.noreply.github.com>
2022-03-29 09:03:47 -0400

    Update CD.java

--- CD.java
+++ CD.java
@@ -1,6 +1,30 @@
-public class CD extends Inventory {
-    public CD(int productPrice, int stockSize) {
-        super(productPrice, stockSize);
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bookstoremanagementproject1;
+
+/**
+ *
+ * @author Paydreanne E. Hinton
+ * instructor Professor Van Custodio
+ * courseSection ITSC1213-106-27949
+ * Bookstore Management Project 1
+ * Subclass for Inventory - CD
+ */
+import java.text.*;
+public class CD extends Inventory implements prodDisplay {
+    DecimalFormat df = new DecimalFormat("#.##");
+    public CD () {
+        super();
     }
-    public CD () {}
+    public CD(int stockSize, double productPrice) {
+        super(stockSize, productPrice);
+    }
+
+    public void productDisplay () {
+        String prodPrice = "CDs cost $" + df.format(this.getProductPrice());
+        String stoSize = "\nThere are currently " + this.getStockSize() + " CDs available";
+        System.out.println(prodPrice + stoSize);
+    }
 }
